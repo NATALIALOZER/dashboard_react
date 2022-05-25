5dd17e9284ca38752113d45b88b336e52ec49934
NATALIALOZER <BADwolf101115@gmail.com>
2022-05-25 22:29:50 +0300

    start of react app: added new files: MainDash + Cards + Card + Sidebar + Table folders

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import "./Card.css";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { motion, AnimateSharedLayout } from "framer-motion";
+import { UilTimes } from "@iconscout/react-unicons";
+import Chart from "react-apexcharts";
+import { chartData } from "../../Data/Data";
+
+const Card = (props) => {
+    const [expanded, setExpanded] = useState(false);
+    return (
+        <AnimateSharedLayout>
+            {expanded ? (
+                <ExpandedCard param={props} setExpanded={() => setExpanded(false)} />
+            ) : (
+                <CompactCard param={props} setExpanded={() => setExpanded(true)} />
+            )}
+        </AnimateSharedLayout>
+    )
+}
+
+const CompactCard = ({param, setExpanded}) => {
+    const Png = param.png;
+    return(
+        <div className="CompactCard"
+        style={{
+            background: param.color.backGround,
+            boxShadow: param.color.boxShadow
+        }}
+        onClick={setExpanded}>
+            <div className="radialBar">
+                <CircularProgressbar
+                value={param.barValue}
+                text={`${param.barValue}%`}
+               />
+               <span>{param.title}</span>
+            </div>
+            <div className="detail">
+                <Png/>
+                <span>${param.value}</span>
+                <span>Last 24 hours</span>
+            </div>
+        </div>
+    )
+}
+
+// Expanded Card
+function ExpandedCard({ param, setExpanded }) {
+    
+    return (
+        <motion.div
+          className="ExpandedCard"
+          style={{
+            background: param.color.backGround,
+            boxShadow: param.color.boxShadow,
+          }}
+          layoutId="expandableCard"
+        >
+          <div style={{ alignSelf: "flex-end", cursor: "pointer", color: "white" }}>
+            <UilTimes onClick={setExpanded} />
+          </div>
+            <span>{param.title}</span>
+          <div className="chartContainer">
+            <Chart options={chartData.options} series={param.series} type="area" />
+          </div>
+          <span>Last 24 hours</span>
+        </motion.div>
+      );
+}
+
+export default Card
